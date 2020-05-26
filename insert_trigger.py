f6c4a78e1cc5e9a088c7b661e1cd1bddc073a88d
TyIL TTY7 <q1342893971@gmail.com>
2020-05-26 21:05:29 +0800

    [add][meta] add sample lines using comment in script `insert_trigger.py`

--- insert_trigger.py
+++ insert_trigger.py
@@ -2,6 +2,32 @@
 # -*- coding: utf-8 -*-
 
 
+def insert_bashrc():
+    # # Recommend to put this at the end of user `.bashrc`.
+    # path_to_file='/path/to/file'
+    # if [ -f "${path_to_file}" ]; then
+    #     source "${path_to_file}"
+    # fi
+    pass
+
+
+def insert_gitconfig():
+    # [include]
+    # ; !! POSITION OF THIS SECTION MATTERS !!
+    # ;
+    # ; The included config key will be overrode
+    # ; by later key with the same name!
+    # ;
+    # ; Therefore it is recommended to put this
+    # ; section at the end of global `.gitconfig`.
+    #     path = /path/to/file
+    pass
+
+
+def insert_vimrc():
+    pass
+
+
 def main():
     pass
 
