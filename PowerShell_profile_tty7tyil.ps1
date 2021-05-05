5514ad9007cfe8a27e4c917abd597174b67d4ec2
TyIL TTY7 <B90FCF15A76E432D@outlook.com>
2021-05-06 02:44:18 +0800

    [add][powershell] set bash like autocomplete

--- PowerShell_profile_tty7tyil.ps1
+++ PowerShell_profile_tty7tyil.ps1
@@ -1,1 +1,3 @@
 Function Prompt {"`n" + "> ${env:UserName} @ ${env:ComputerName} [$(Get-Date -UFormat '%Y-%m-%d %H:%M:%S %Z')] In [$(@(Get-History).Count + 1)] :`n" + "- $($ExecutionContext.SessionState.Path.CurrentLocation)`n" + $(if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {"# "} else {"$ "})}
+
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
