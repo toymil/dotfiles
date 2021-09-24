6755f612fb262be2624dccde1dc10d2950f47a46
TyIL TTY7 <B90FCF15A76E432D@outlook.com>
2021-09-24 11:09:46 +0800

    [add][powershell] add usual scrcpy command as alias

--- tty7tyil.PowerShell_profile.ps1
+++ tty7tyil.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
-Function Prompt {"`n" + "> ${env:UserName} @ ${env:ComputerName} [$(Get-Date -UFormat '%Y-%m-%d %H:%M:%S %Z')] In [$(@(Get-History).Count + 1)] :`n" + "- $($ExecutionContext.SessionState.Path.CurrentLocation)`n" + $(if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {"# "} else {"$ "})}
+function Prompt {"`n" + "> ${env:UserName} @ ${env:ComputerName} [$(Get-Date -UFormat '%Y-%m-%d %H:%M:%S %Z')] In [$(@(Get-History).Count + 1)] :`n" + "- $($ExecutionContext.SessionState.Path.CurrentLocation)`n" + $(if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {"# "} else {"$ "})}
 
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
+
+function cs {scrcpy --bit-rate 8m --stay-awake @Args}
+function cso {cs --turn-screen-off @Args}
