e85f660e051fbdfcdc442f64c1825b0b06a54eee
TyIL TTY7 <B90FCF15A76E432D@outlook.com>
2021-10-20 19:08:28 +0800

    [add][powershell] add `ls` alias based on `lsd` (since `exa` is not available on windows yet)

--- tty7tyil.PowerShell_profile.ps1
+++ tty7tyil.PowerShell_profile.ps1
@@ -2,5 +2,10 @@ function Prompt {"`n" + "> ${env:UserName} @ ${env:ComputerName} [$(Get-Date -UF
 
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
+function e {lsd -AF --color=always --group-dirs=first --icon=never @Args}
+function el {e -hl --blocks='permission,size,user,date,name' --date='+%Y-%m-%d %H:%M %z' @Args}
+function ell {e -ahl --blocks='inode,permission,size,user,group,date,name' --date='+%Y-%m-%d %H:%M:%S %z' @Args}
+function et {e --tree @Args}
+
 function cs {scrcpy --bit-rate 8m --stay-awake @Args}
 function cso {cs --turn-screen-off @Args}
