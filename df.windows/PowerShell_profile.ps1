894fa6edbc9a6b759d09ff9fe01c50bbecd04c49
SPQV MF <B90FCF15A76E432D@outlook.com>
2023-03-04 18:11:25 +0800

    [add][shell][posh] port `ls`, `mkdir`, `cp`, `mv`, `rm` aliases to windows posh with uutils-coreutils

--- df.windows/PowerShell_profile.ps1
+++ df.windows/PowerShell_profile.ps1
@@ -2,6 +2,10 @@ function Prompt { "`n" + "> ${env:UserName} @ ${env:ComputerName} [$(Get-Date -U
 
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
+function l { coreutils.exe ls -AF --color=always --group-directories-first -N @Args }
+function ll { coreutils.exe ls -AF --color=always --group-directories-first -GhlN --time-style="+%Y-%m-%d %H %:::z" @Args }
+function lll { coreutils.exe ls -AF --color=always --group-directories-first -ahil --time-style="+%Y-%m-%d %H:%M:%S %:z" @Args }
+
 function e { lsd -AF --color=always --group-dirs=first --icon=never @Args }
 function el { e -hl --blocks='permission,size,user,date,name' --date='+%Y-%m-%d %H:%M %z' @Args }
 function ell { e -ahl --blocks='inode,permission,size,user,group,date,name' --date='+%Y-%m-%d %H:%M:%S %z' @Args }
@@ -9,3 +13,12 @@ function et { e --tree @Args }
 
 function cs { scrcpy --bit-rate 8m @Args }
 function cso { cs --turn-screen-off @Args }
+
+function mkv { coreutils.exe mkdir -vp @Args }
+
+function cpv { coreutils.exe cp -v @Args }
+function mvv { coreutils.exe mv -v @Args }
+
+function rmv { coreutils.exe rm -vIr @Args }
+function rmf { coreutils.exe rm -vIrf @Args }
+function rmi { coreutils.exe rm -ir @Args }
