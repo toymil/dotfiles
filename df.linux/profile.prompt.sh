8bf28525987df21bbd1557244f83487878a39bd0
SPQV MF <B90FCF15A76E432D@outlook.com>
2023-06-29 19:32:57 +0800

    [add][vim][shell][linux] add "small" variants of shell profile and vim config

---
+++ df.linux/profile.prompt.sh
@@ -0,0 +1,59 @@
+################################################################################
+#                            CUSTOMIZE BASH PROMPT                             #
+################################################################################
+
+# `\[` - begin sequence of non-printing characters
+# `\]` - end sequence of non-printing characters
+# used for bash to correctly calculates the size of the prompt
+bnp="\["
+enp="\]"
+
+b_title="${bnp}\e]0;"
+e_title="\a${enp}"
+b_command="\$("
+e_command=")"
+
+# more information: https://en.wikipedia.org/wiki/ANSI_escape_code
+# color sequence format:`[<PREFIX>];[<COLOR>];[<TEXT DECORATION>]`
+#   prefix 256 colors foreground: `38;5`
+#   prefix 256 colors background: `48;5`
+#   decoration bold: `1`
+#   decoration underline: `4`
+bcs="${bnp}\e["  # begin control sequence
+ecs="m${enp}"  # end control sequence
+reset_color="${bcs}0${ecs}"
+
+directory="\w"
+hostname="\h"
+username="\u"
+
+# normal prompt
+title="\
+${b_title}\
+${username}@${hostname} [${directory}]\
+${e_title}\
+"
+
+line_1="\
+${reset_color}\n\
+${bcs}38;5;213;1${ecs}> \
+${bcs}38;5;81${ecs}${username} \
+${bcs}38;5;7${ecs}@ \
+${bcs}38;5;84${ecs}${hostname} \
+${reset_color}${bcs}38;5;7${ecs}:\
+${reset_color}\n\
+"
+line_2="\
+${bcs}38;5;213;1${ecs}- \
+${reset_color}${bcs}38;5;229${ecs}${directory}\
+${reset_color}\n\
+"
+line_3="\
+${bcs}38;5;202;1${ecs}\\$ \
+${reset_color}${bcs}38;5;253${ecs}\
+"
+
+prompt_normal="${title}${line_1}${line_2}${line_3}"
+
+# set prompt
+export PS1="${prompt_normal}"
---
+++ df.linux/profile.rest.sh
@@ -0,0 +1,109 @@
+################################################################################
+#                             ENVIRONMENT VARIABLE                             #
+################################################################################
+
+# set default editor to `vim`
+export EDITOR=vim
+export VISUAL="${EDITOR}"
+export SUDO_EDITOR="${EDITOR}"
+
+
+# ################################################################################
+# #                                BASH BEHAVIOUR                                #
+# ################################################################################
+# 
+# # includes filenames beginning with a '.' in the results of filename expansion
+# shopt -s dotglob
+# 
+# #===========================================================
+# #|                    set bash history                    ||
+# 
+# export HISTTIMEFORMAT="[%F %T %z]  "
+
+
+################################################################################
+#                                   ALIASES                                    #
+################################################################################
+
+alias "sudo"="sudo "
+
+#===========================================================
+#|          modernize/customize `ls` et. `tree`           ||
+
+ls_time_style_short="+\
+%Y-%m-%d %H %:::z
+--%m-%d %H:%M %:::z\
+"
+ls_time_style_long="+\
+%Y-%m-%d %H:%M:%S %:z
+   --%m-%d %H:%M:%S %:z\
+"
+ls_base="\
+ls \
+-AF \
+--color=always \
+--group-directories-first\
+"
+l="${ls_base} -N"
+ll="${ls_base} -GhlN --time-style='${ls_time_style_short}'"
+lll="${ls_base} -ahil --time-style='${ls_time_style_long}'"
+
+alias   "l"="LC_COLLATE=C.UTF-8 ${l}"
+alias  "ll"="LC_COLLATE=C.UTF-8 ${ll}"
+alias "lll"="LC_COLLATE=C.UTF-8 ${lll}"
+alias "lle"="LC_COLLATE=en_US.utf8 ${ll}"
+alias "llz"="LC_COLLATE=zh_CN.utf8 ${ll}"
+
+alias  "ct"="LC_COLLATE=C.UTF-8 tree -aFC --dirsfirst"
+alias "ctp"="ct -fpugi"
+alias "cts"="ct -sh --du"
+
+#===========================================================
+#|           modernize/customize `cp` et. `scp`           ||
+
+alias "crs-dae"="rsync --daemon --no-detach"
+
+alias "crs"="\
+rsync \
+--human-readable \
+--info=progress2 \
+--info=stats \
+\
+--archive \
+--no-devices \
+--no-specials \
+\
+--whole-file \
+--preallocate \
+--no-compress\
+"
+
+alias "crs-remote"="\
+crs \
+--verbose \
+--info=skip \
+--safe-links \
+--no-perms \
+--no-group \
+--no-owner\
+"
+
+alias "crs-remote-644"="crs-remote --chmod=D0755,F0644"
+
+#===========================================================
+#|                    customize `dig`                     ||
+
+alias "cdig"="dig +all +nocookie"
+alias "cdigs"="cdig +short"
+
+#===========================================================
+#|                     q.o.l. aliases                     ||
+
+alias "mkv"="mkdir -vp"
+
+alias "cpv"="cp -v"
+alias "mvv"="mv -v"
+
+alias "rmv"="rm -vIr"
+alias "rmf"="rm -vIrf"
+alias "rmi"="rm -ir"
