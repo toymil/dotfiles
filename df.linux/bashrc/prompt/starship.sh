ebbe72d0fa0ebd8ff4b01702eca135ca33757d24
SPQV MF <B90FCF15A76E432D@outlook.com>
2023-11-20 22:07:57 +0800

    mod(shell, bash, starship): modify title to include current dir name

--- df.linux/bashrc/prompt/starship.sh
+++ df.linux/bashrc/prompt/starship.sh
@@ -3,6 +3,11 @@
 eval "$(starship init bash)"
 
 function set_title() {
-    echo -ne "\e]0; $USER @ $HOSTNAME : $PWD \a"
+    path_full=$PWD
+
+    path_last_component=${path_full##*/}
+    path_last_component=${path_last_component:-/}
+
+    echo -ne "\e]0; $USER @ $HOSTNAME : [ $path_last_component ]( $path_full ) \a"
 }
 starship_precmd_user_func="set_title"
