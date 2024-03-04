3a8b5191ff139634e1fd816e7726205e698750bb
SPQV MF <B90FCF15A76E432D@outlook.com>
2024-03-05 01:33:13 +0800

    add: more shopt configs

--- home.linux/.bashrc.d/shopt.sh
+++ home.linux/.bashrc.d/shopt.sh
@@ -3,3 +3,9 @@
 # see: https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
 
 shopt -s dotglob
+shopt -s failglob
+shopt -s globasciiranges
+shopt -s globskipdots
+
+shopt -u nocaseglob
+shopt -u nullglob
