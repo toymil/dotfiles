1e0296b133c3c7cd03cafda2b2c0bfb5ee5c4104
SPQV MF <B90FCF15A76E432D@outlook.com>
2024-03-05 01:14:16 +0800

    sync

--- home.linux/.bashrc.d/history.sh
+++ home.linux/.bashrc.d/history.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# includes filenames beginning with a '.' in the results of filename expansion
-shopt -s dotglob
-
 #===========================================================
 #|                    set bash history                    ||
 
---
+++ home.linux/.bashrc.d/shopt.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# includes filenames beginning with a '.' in the results of filename expansion
+shopt -s dotglob
