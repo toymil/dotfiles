c47ce9f80da9c2a9116b2c000c49754b8f2afb1d
SPQV MF <B90FCF15A76E432D@outlook.com>
2024-03-05 00:49:05 +0800

    prog: split debian package manager (apt, dpkg) specific aliases to debian folder

---
+++ home.linux.debian/.bashrc.d/alias.package_manager.sh
@@ -0,0 +1,18 @@
+alias "apt-update"="\
+apt update \
+&& apt upgrade \
+&& apt full-upgrade \
+&& apt install -f\
+"
+
+alias "apt-clean"="\
+apt autoremove \
+&& apt clean \
+&& dpkg -l | grep '^rc' | awk '{print \$2}' | xargs dpkg --purge\
+"
+
+alias "pkg-ls-obsolete"="apt list '~o'"
+alias "pkg-ls-non-debian"="apt list '?narrow(?installed, ?not(?origin(Debian)))'"
+alias "pkg-ls-leftover-config"="find /etc -name '*.dpkg-*' -o -name '*.ucf-*' -o -name '*.merge-error'"
+
+# TODO: find dummy packages with `deborphan` (see debian release note "Upgrades" chapter)
--- home.linux/.bashrc.d/alias.sh
+++ home.linux/.bashrc.d/alias.sh
@@ -213,23 +213,6 @@ alias "cdigs"="cdig +short"
 #===========================================================
 #|                'update' q.o.l. aliases                 ||
 
-# apt update
-alias "apt-update"="\
-apt update \
-&& apt upgrade \
-&& apt full-upgrade \
-&& apt install -f\
-"
-alias "apt-clean"="\
-apt autoremove \
-&& apt clean \
-&& dpkg -l | grep '^rc' | awk '{print \$2}' | xargs dpkg --purge\
-"
-alias "pkg-ls-obsolete"="apt list '~o'"
-alias "pkg-ls-non-debian"="apt list '?narrow(?installed, ?not(?origin(Debian)))'"
-alias "pkg-ls-leftover-config"="find /etc -name '*.dpkg-*' -o -name '*.ucf-*' -o -name '*.merge-error'"
-# TODO: find dummy packages with `deborphan` (see debian release note "Upgrades" chapter)
-
 # flatpak update
 alias "flat-update"="flatpak --user update"
 alias "flat-clean"="flatpak --user repair && flatpak --user uninstall --unused"
