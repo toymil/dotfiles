0dd1f3087ec6eb132b075a13217e82baa4c5d05d
BCA4 <B90FCF15A76E432D@outlook.com>
2024-03-24 21:34:43 +0800

    add(nvim): basic neovim config

---
+++ home/.config/nvim/lua/base.lua
@@ -0,0 +1,71 @@
+vim.opt.encoding = "utf-8"
+-- vim.opt.fileencodings = {
+--     "ucs-bom",
+--     "utf-8",
+--     "default",
+--     "latin1",
+-- }
+
+
+vim.opt.undodir = "."
+vim.opt.undofile = true
+vim.opt.undolevels = 512
+
+
+-- see: https://neovim.io/doc/user/options.html#'tabstop'
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+
+
+
+
+
+vim.opt.filetype = "on"
+vim.opt.syntax = "on"
+
+
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = "|-",
+    trail = "-",
+    nbsp = "~",
+
+    extends = ">",
+    precedes = "<",
+}
+
+
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = { "screenline", "number" }
+vim.opt.colorcolumn = { "50", "80", "100", "120" }
+
+
+vim.opt.number = true
+vim.opt.ruler = true
+vim.opt.laststatus = 2
+
+
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 10
+
+vim.opt.wrap = false
+
+vim.opt.sidescroll = 10
+
+vim.opt.whichwrap:append("<")
+vim.opt.whichwrap:append(">")
+vim.opt.whichwrap:append("[")
+vim.opt.whichwrap:append("]")
+
+
+
+
+
+vim.opt.termguicolors = true
+
+vim.cmd.colorscheme("delek")
