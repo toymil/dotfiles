2ee052da3392a161d4d68f551df727efa1750134
BCA4 <B90FCF15A76E432D@outlook.com>
2024-04-14 20:50:09 +0800

    add(shell, alias, suse): add zypper update alias

--- home.linux.suse/.bashrc.d/alias.package_manager.sh
+++ home.linux.suse/.bashrc.d/alias.package_manager.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+alias "zp-update"="\
+zypper refresh \
+&& zypper dist-upgrade \
+&& zypper install-new-recommends --no-recommends\
+"
+
+alias "zp-rm"="zypper remove --clean-deps"
+alias "pkg-ls-unneeded"="zypper packages --unneeded"
+
+alias "pkg-ls-orphaned"="zypper packages --orphaned"
