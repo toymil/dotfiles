ac3ee378b321710571a4dd5e53cef6630ae20060
BCA4 <B90FCF15A76E432D@outlook.com>
2024-07-26 20:25:48 +0800

    chore

--- home.linux/.bashrc.d/alias.sh
+++ home.linux/.bashrc.d/alias.sh
@@ -124,9 +124,11 @@ alias "etl"="et \
 "
 
 alias "etg"="et --git-ignore"
+
 alias "et1"="et -L 1"
 alias "et2"="et -L 2"
 alias "et3"="et -L 3"
+
 alias "etl1"="etl -L 1"
 alias "etl2"="etl -L 2"
 alias "etl3"="etl -L 3"
