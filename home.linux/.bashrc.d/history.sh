2e1557ca5aa507f69c423cba32f9c5aae9fe0e92
BCA4 <B90FCF15A76E432D@outlook.com>
2024-07-27 19:51:42 +0800

    mod(shell, bash, history): pick some changes from sens branch

--- home.linux/.bashrc.d/history.sh
+++ home.linux/.bashrc.d/history.sh
@@ -3,4 +3,8 @@
 #===========================================================
 #|                    set bash history                    ||
 
+export HISTCONTROL=""
+export HISTIGNORE="exit:clear"
 export HISTTIMEFORMAT="[%F %T %z]  "
+shopt -s histappend
+export PROMPT_COMMAND+=('history -a')
