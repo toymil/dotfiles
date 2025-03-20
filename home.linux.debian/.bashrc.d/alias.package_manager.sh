fc31f2e648acd42fb06801466509fec62e0afac3
BCA4 <B90FCF15A76E432D@outlook.com>
2025-03-20 21:32:54 +0800

    mod(shell): pickup sudo apt aliases from another branch

--- home.linux.debian/.bashrc.d/alias.package_manager.sh
+++ home.linux.debian/.bashrc.d/alias.package_manager.sh
@@ -18,3 +18,21 @@ alias "pkg-ls-non-debian"="apt list '?narrow(?installed, ?not(?origin(Debian)))'
 alias "pkg-ls-leftover-config"="find /etc -name '*.dpkg-*' -o -name '*.ucf-*' -o -name '*.merge-error'"
 
 # TODO: find dummy packages with `deborphan` (see debian release note "Upgrades" chapter)
+
+
+if [ ${UID} -ne 0 ] ; then
+
+alias "apt-update"="\
+sudo apt update \
+&& sudo apt upgrade \
+&& sudo apt full-upgrade \
+&& sudo apt install -f\
+"
+
+alias "apt-clean"="\
+sudo apt autoremove \
+&& sudo apt clean \
+&& sudo dpkg -l | grep '^rc' | awk '{print \$2}' | xargs sudo dpkg --purge\
+"
+
+fi
