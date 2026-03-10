e14da52005da0057d82d7e1810378f081597a17f
BCA4 <B90FCF15A76E432D@outlook.com>
2026-03-11 03:46:52 +0800

    bash(hist): exclude `history` command itself from history

--- home.linux/.bashrc.d/history.sh
+++ home.linux/.bashrc.d/history.sh
@@ -4,7 +4,7 @@
 #|                    set bash history                    ||
 
 export HISTCONTROL=""
-export HISTIGNORE="exit:clear"
+export HISTIGNORE="exit:clear:history|*:history |*"
 export HISTTIMEFORMAT="[%F %T %z]  "
 shopt -s histappend
 export PROMPT_COMMAND+=('history -a')
