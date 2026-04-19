0c276aadc09e8a6f0344a7777c3775dd838f671c
BCA4 <B90FCF15A76E432D@outlook.com>
2026-04-20 05:15:38 +0800

    update history.sh format

--- home.linux/.bashrc.d/20-history.sh
+++ home.linux/.bashrc.d/20-history.sh
@@ -1,10 +1,26 @@
-#!/usr/bin/env bash
+dotfiles__histignore_base() {
+    export HISTIGNORE="exit:clear"
 
-#===========================================================
-#|                    set bash history                    ||
+    export HISTIGNORE="${HISTIGNORE}:reboot:sudo reboot"
+    export HISTIGNORE="${HISTIGNORE}:shutdown *:sudo shutdown *"
 
-export HISTCONTROL=""
-export HISTIGNORE="exit:clear:history|*:history |*"
-export HISTTIMEFORMAT="[%F %T %z]  "
-shopt -s histappend
-export PROMPT_COMMAND+=('history -a')
+    export HISTIGNORE="${HISTIGNORE}:history|*:history |*"
+
+    unset -f dotfiles__histignore_base
+}
+
+
+dotfiles__main() {
+    ## enable command history
+    #set -o history
+
+    export HISTCONTROL=""
+    dotfiles__histignore_base
+    export HISTTIMEFORMAT="[%F %T %z]  "
+
+    shopt -s histappend
+    # append to HISTFILE before each prompt
+    export PROMPT_COMMAND+=('history -a')
+}
+dotfiles__main
+unset -f dotfiles__main
