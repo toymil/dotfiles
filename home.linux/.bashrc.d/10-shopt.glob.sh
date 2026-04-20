2556d86a9c5b77b9e02281c6f7fb865d0a0b7100
BCA4 <B90FCF15A76E432D@outlook.com>
2026-04-21 02:30:51 +0800

    chore

--- home.linux/.bashrc.d/10-shopt.glob.sh
+++ home.linux/.bashrc.d/10-shopt.glob.sh
@@ -1,7 +1,7 @@
 # see: https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
 
 shopt -s dotglob
-# shopt -s failglob
+#shopt -s failglob
 shopt -s globasciiranges
 shopt -s globskipdots
 
