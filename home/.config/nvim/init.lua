36b19fbc50d88408fb9c7d87b50a62c40dfa77a7
BCA4 <B90FCF15A76E432D@outlook.com>
2026-05-13 14:50:56 +0800

    nvim: remove plugin config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,3 +1,2 @@
 require("base")
 require("chore")
-require("plugins")
--- home/.config/nvim/lua/plugins.lua
+++
@@ -1,6 +0,0 @@
--- where to put package files:
--- https://neovim.io/doc/user/repeat.html#packages
-
-
--- https://github.com/rebelot/kanagawa.nvim
-vim.cmd.colorscheme("kanagawa-wave")
