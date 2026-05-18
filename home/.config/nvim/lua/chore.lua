2fad4da3a613ce92df47a05bd6f0fb0030a9acae
BCA4 <B90FCF15A76E432D@outlook.com>
2026-05-18 23:25:34 +0800

    nvim: no chore for popup-menu needed

--- home/.config/nvim/lua/chore.lua
+++
@@ -1,5 +0,0 @@
--- remove the "How-to disable mouse" menu item
--- and the separator above it
--- see: https://neovim.io/doc/user/vim_diff.html#_default-mouse
-vim.cmd.aunmenu("PopUp.How-to\\ disable\\ mouse")
-vim.cmd.aunmenu("PopUp.-1-")
